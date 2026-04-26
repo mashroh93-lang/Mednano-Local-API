eee620e01e024802dbace7b334df6707747aa723
mashroh93-lang <mashroh93@gmail.com>
2026-04-26 18:35:11 +0300

    Update download_service.dart

--- mednano_vision_app/lib/download_service.dart
+++ mednano_vision_app/lib/download_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart'; 
 
 class DownloadService {
   final String fileUrl = "https://github.com/mashroh93-lang/Mednano-Local-API/releases/download/v1.0.0/mednano_edge.ptl";
@@ -10,19 +11,54 @@ class DownloadService {
     return "${directory.path}/mednano_edge.ptl";
   }
 
+  
   Future<bool> isModelDownloaded() async {
     final path = await getLocalPath();
     return File(path).exists();
   }
 
-  Future<void> downloadModel({required Function(double) onProgress}) async {
+  
+  Future<bool> downloadModel({required Function(double) onProgress}) async {
     try {
       final savePath = await getLocalPath();
       await Dio().download(fileUrl, savePath, onReceiveProgress: (rec, total) {
         if (total != -1) onProgress(rec / total);
       });
+      
+     
+      if (await File(savePath).length() > 0) {
+        return true; 
+      } else {
+        return false; 
+      }
     } catch (e) {
       print("Download Error: $e");
+      return false; 
+    }
+  }
+
+  
+  Future<bool> importModelFromDevice() async {
+    try {
+     
+      FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+      if (result != null && result.files.single.path != null) {
+        File sourceFile = File(result.files.single.path!);
+        String destPath = await getLocalPath();
+        
+
+        await sourceFile.copy(destPath);
+        
+         
+        if (await File(destPath).exists()) {
+          return true;
+        }
+      }
+      return false; 
+    } catch (e) {
+      print("Import Error: $e");
+      return false;
     }
   }
 }
